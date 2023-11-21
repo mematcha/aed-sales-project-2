26c7d122ada12b1839b123ec2d3213894941daff
matchasat <sathwikmatcha.1899@gmail.com>
2023-11-20 21:16:07 -0500

    Minor Data Changes

--- RangePricingApplication/src/TheBusiness/ProductManagement/Product.java
+++ RangePricingApplication/src/TheBusiness/ProductManagement/Product.java
@@ -13,23 +13,28 @@ import TheBusiness.OrderManagement.OrderItem;
  * @author kal bugrara
  */
 public class Product {
+
     private String name;
 
-    public String getName() {
-        return name;
-    }
     private int floorPrice;
     private int ceilingPrice;
     private int targetPrice;
+    
     ArrayList<OrderItem> orderitems;
-        public Product( int fp, int cp, int tp) {
+
+    public Product(int fp, int cp, int tp) {
 
         floorPrice = fp;
         ceilingPrice = cp;
         targetPrice = tp;
         orderitems = new ArrayList();
-        
+
     }
+
+    public String getName() {
+        return name;
+    }
+
     public Product(String n, int fp, int cp, int tp) {
         name = n;
         floorPrice = fp;
@@ -37,36 +42,49 @@ public class Product {
         targetPrice = tp;
         orderitems = new ArrayList();
     }
-        public Product updateProduct(int fp, int cp, int tp) {
+
+    public Product updateProduct(int fp, int cp, int tp) {
         floorPrice = fp;
         ceilingPrice = cp;
         targetPrice = tp;
         return this; //returns itself
     }
-    public int getTargetPrice() {return targetPrice;}
-    public void addOrderItem(OrderItem oi){     
+
+    public int getTargetPrice() {
+        return targetPrice;
+    }
+
+    public void addOrderItem(OrderItem oi) {
         orderitems.add(oi);
     }
+
     //Number of item sales above target 
-    public int getNumberOfProductSalesAboveTarget(){
+    public int getNumberOfProductSalesAboveTarget() {
         int sum = 0;
-        for (OrderItem oi: orderitems){
-            if(oi.isActualAboveTarget()==true) sum = sum +1;
+        for (OrderItem oi : orderitems) {
+            if (oi.isActualAboveTarget() == true) {
+                sum = sum + 1;
+            }
         }
         return sum;
     }
-    public int getNumberOfProductSalesBelowTarget(){
+
+    public int getNumberOfProductSalesBelowTarget() {
         int sum = 0;
-        for (OrderItem oi: orderitems){
-            if(oi.isActualBelowTarget()==true) sum = sum +1;
+        for (OrderItem oi : orderitems) {
+            if (oi.isActualBelowTarget() == true) {
+                sum = sum + 1;
+            }
         }
         return sum;
-    }    
-    
-        public boolean isProductAlwaysAboveTarget(){
-        
-        for (OrderItem oi: orderitems){
-            if(oi.isActualAboveTarget()==false) return false; //
+    }
+
+    public boolean isProductAlwaysAboveTarget() {
+
+        for (OrderItem oi : orderitems) {
+            if (oi.isActualAboveTarget() == false) {
+                return false; //
+            }
         }
         return true;
     }
@@ -74,32 +92,37 @@ public class Product {
     //For example, if target is at $2000 and actual is $2500 then revenue gained
     // is $500 above the expected target. If the actual is $1800 then the lose will be $200
     // Add all these difference to get the total including wins and loses
-    
-        public int getOrderPricePerformance() {
+
+    public int getOrderPricePerformance() {
         int sum = 0;
         for (OrderItem oi : orderitems) {
             sum = sum + oi.calculatePricePerformance();     //positive and negative values       
         }
         return sum;
     }
-        public int getSalesVolume() {
+
+    public int getSalesVolume() {
         int sum = 0;
         for (OrderItem oi : orderitems) {
             sum = sum + oi.getOrderItemTotal();     //positive and negative values       
         }
         return sum;
     }
-    public void setName(String n){
+
+    public void setName(String n) {
         name = n;
     }
+
     @Override
-    public String toString(){
+    public String toString() {
         return name;
     }
-    public int getFloorPrice(){
+
+    public int getFloorPrice() {
         return floorPrice;
     }
-    public int getCeilingPrice(){
+
+    public int getCeilingPrice() {
         return ceilingPrice;
     }
 
